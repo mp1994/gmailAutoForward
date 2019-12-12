6669893cb77e1e66ad4b3bf04e4959bd505791bc
mp1994 <mp.1994@icloud.com>
2019-12-12 11:21:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+#!/usr/local/bin/python3
+
+import poplib
+import smtplib
+import string, random
+from io import StringIO
+import email
+import logging
+from email.mime.text import MIMEText
+
+# POP3 Server (default: gmail)
+SERVER_POP3 = "pop.gmail.com"
+# USERNAME 
+#USER  = ""
+# PASSWORD
+PASSWORD = ""
+
+runFlag = 1
+while runFlag:
+    
+    # connect to server
+    logging.debug('connecting to ' + SERVER_POP3)
+    server = poplib.POP3_SSL(SERVER_POP3)
+
+    # log in
+    logging.debug('log in')
+    server.user(USER)
+    server.pass_(PASSWORD)
+
+    # list items on server
+    logging.debug('listing emails')
+    resp, items, octets = server.list()
+    index = len(items)
+        
+    # Check if there are unread messages
+    if index:
+        runFlag = 0
+    else:
+        continue
+    
+    # Latest e-mail
+    raw_email  = b"\n".join(server.retr(index)[1])
+    parsed_email = email.message_from_bytes(raw_email)
+
+    # E-mail data
+    oggetto = parsed_email['Subject']
+    contenuto = parsed_email.get_payload()
+
+    server.quit()
+    logging.debug('connection closed')
+
+# Set a filter on the e-mail subject (optional)
+if oggetto != '[My Filter]':
+    exit()    
+
+# Set a filter on the sender
+if parsed_email['From'] != 'foo.bar@test.com':
+  exit()
+
+# Forward the (right) e-mail!
+SERVER_SMTP = "smtp.gmail.com"
+
+# SMTP connection
+server = smtplib.SMTP_SSL(SERVER_SMTP, 465)
+server.login(USER,PASSWORD)
+server.set_debuglevel(1)
+
+# SEND TO
+# Uncomment only one of the following options
+# Send to a single recepient...
+#TO = "dest1@example.com"
+# ... or send to a list
+TO = ['dest2@example.com', 
+'dest3@example.com', 
+'dest4@example.com'] 
+
+# Prepare the content
+msg = MIMEText(contenuto)
+
+# Add the subject
+msg['Subject'] = oggetto
+msg['To'] = ", ".join(TO)
+
+# Send the e-mail
+server.sendmail(USER, TO, msg.as_string())
+
+# Close connection
+server.quit()
